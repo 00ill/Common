57696fbb42016e50072d4535638b28f9277ae24b
00ill <101080818+00ill@users.noreply.github.com>
2025-02-28 15:06:12 +0900

    Update Utils.c

--- Utils/Utils.c
+++ Utils/Utils.c
@@ -1,44 +1,85 @@
 #include "Utils.h"
 
-/// @brief Change the value in proportion to speed, change immediately when speed is 1
-/// @param type type of value, choose 'i', 'f', 'l, 'L'
-/// @param value current value
-/// @param target_value targer value
+/// @brief Clamps the given value between a minimum and maximum value.
+/// @param targetValue The value to be clamped.
+/// @param minValue The minimum value that targetValue can be.
+/// @param maxValue The maximum value that targetValue can be.
+/// @return Returns minValue if targetValue is less than minValue, maxValue if targetValue is greater than maxValue, and targetValue otherwise.
+float ClampValue(float targetValue, float minValue, float maxValue)
+{
+    if (targetValue < minValue)
+    {
+        return minValue;
+    }
+    else if (targetValue > maxValue)
+    {
+        return maxValue;
+    }
+    else
+    {
+        return targetValue;
+    }
+}
+
+/// @brief Change the*current in proportion to speed, change immediately when speed is 1
+/// @param type type of*current, choose 'i', 'f', 'l, 'L'
+/// @param*current _current
+/// @param target targer*current
 /// @param speed change speed 0 ~ 1
-void lerp(char type, void *value, float target_value, float speed)
+void Lerp(char type, void *current, float target, float speed)
 {
     switch (type)
     {
     case 'i':
     {
-        int *val = (int *)value;
-        float current_value = (float)(*val);
-        float delta = (target_value - current_value) * speed;
+        int *val = (int)*current;
+        float _current = (float)(*val);
+        float delta = (target - _current) * speed;
         if (delta > 0 && delta < 1)
             delta = 1;
         if (delta < 0 && delta > -1)
             delta = -1;
-        current_value += delta;
-        *val = (int)current_value;
+        _current += delta;
+        *val = (int)_current;
         break;
     }
     case 'f':
     {
-        float *val = (float *)value;
-        *val += (target_value - *val) * speed;
+        float *val = (float)*current;
+        *val += (target - *val) * speed;
         break;
     }
     case 'l':
     {
-        long *val = (long *)value;
-        *val += (target_value - *val) * speed;
+        long *val = (long)*current;
+        *val += (target - *val) * speed;
         break;
     }
     case 'L':
     {
-        long long *val = (long long *)value;
-        *val += (target_value - *val) * speed;
+        long long *val = (long long)*current;
+        *val += (target - *val) * speed;
         break;
     }
+    }
+}
+
+/// @brief Gradually modifies the current value to approach the target value.
+/// @param current Pointer to the current value that will be modified.
+/// @param target The target value to approach.
+/// @param deltaValue The incremental change applied to the current value.
+void StepModify(float *current, float target, float deltaValue)
+{
+    if (*current < target - deltaValue * 0.5)
+    {
+        *current += deltaValue;
+    }
+    else if (*current > target + deltaValue * 0.5)
+    {
+        *current -= deltaValue;
+    }
+    else
+    {
+        *current = target;
     }
 }
