831ad700e8bbd9ac5f7c956b712a02edaac435ba
00ill <101080818+00ill@users.noreply.github.com>
2025-02-28 15:06:21 +0900

    Update Utils.h

--- Utils/Utils.h
+++ Utils/Utils.h
@@ -1,6 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-void lerp(char type, void *value, float target_value, float speed);
-
+float ClampValue(float targetValue, float minValue, float maxValue);
+void Lerp(char type, void *value, float target_value, float speed);
+void StepModify(float *current, float target, float deltaValue);
 #endif
